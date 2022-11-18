c40ed2b2e4bdbcc5e6dd1ae85cde06f9b56c22f2
nko <epizarro001@ikasle.ehu.eus>
2022-11-18 01:17:21 +0100

    Testing.

--- index.js
+++ index.js
@@ -7,10 +7,28 @@ const fs = require("fs");
 app.use(express.static("public"));
 //Recibir objetos del cliente (extended:true, cualquier objeto)
 app.use(express.urlencoded({extended:true}));
+app.use(express.json())
 
-/*
-app.post("/test", (req, res) => {
-    res.send("Patatata")
-})*/
+//Download from Server:
+app.get("/download", (req, res) => {
+    fs.readFile("public/data.txt", 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(data)
+        res.json(JSON.parse(data))
+    });
+})
+
+//Upload to Server:
+app.post("/upload", (req, res) => {
+    fs.writeFile("public/data.txt", JSON.stringify(req.body), function(err) {
+        if(err){
+            return console.log(err);
+        }
+    })
+    res.send("Successfully received.")
+})
 
 app.listen(3000, function() {console.log("Servidor lanzado en el puerto 3000")});
--- public/js/main.js
+++ public/js/main.js
@@ -2,17 +2,18 @@ window.onload = () => {
     let input = document.getElementById("newTask")
     let list = document.getElementById("task_list")
     let deleteBTN = document.getElementById("deleteAll")
+    let downloadBTN = document.getElementById("download")
+    let uploadBTN = document.getElementById("upload")
 
     let printList = () => {
         list.innerHTML = ''
         let tasks = JSON.parse(localStorage.getItem('tasks'))
-        if(tasks != null) {
+        if(tasks != null)
             tasks.task_list.map(task => {
                 let elm = document.createElement('li')
                 elm.textContent = task
                 list.appendChild(elm)
             })
-        }
     }
 
     printList()
@@ -41,6 +42,33 @@ window.onload = () => {
     deleteBTN.addEventListener('click', e => {
         localStorage.removeItem("tasks")
         list.innerHTML = ''
+    })
+
+    //Download from server:
+    downloadBTN.addEventListener('click', e => {
+        fetch("/download").then(r => r.json()).then(r => {
+            if(Object.keys(r).length < 1)
+                r = {
+                    "task_list": []
+                }
+            localStorage.setItem('tasks', JSON.stringify(r))
+            printList()
+            console.log(r)
+        })
+
+    })
+
+    //Upload to server:
+    uploadBTN.addEventListener('click', e => {
+        fetch('/upload', {
+            method: 'POST',
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: localStorage.getItem('tasks')
+        })
+            .then(r => r.text())
+            .then(r => console.log(r))
     })
 }
 
